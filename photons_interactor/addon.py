d9d95ad186cdbb66f00543ccd3666af3913a99fe
Stephen Moore <stephen@delfick.com>
2018-06-30 15:59:13 +1000

    Make this DeviceFinder adjustment available to tests

--- photons_interactor/addon.py
+++ photons_interactor/addon.py
@@ -2,7 +2,6 @@ from photons_interactor.options import Options
 from photons_interactor.server import Server
 
 from photons_app.formatter import MergedOptionStringFormatter
-from photons_device_finder import DeviceFinder
 from photons_app.actions import an_action
 
 from option_merge_addons import option_merge_addon_hook
@@ -11,7 +10,6 @@ from input_algorithms.meta import Meta
 
 @option_merge_addon_hook(extras=[("lifx.photons", "__all__")])
 def __lifx__(collector, *args, **kwargs):
-    DeviceFinder._merged_options_formattable = True
     collector.register_converters(
           { (0, ("interactor", )): Options.FieldSpec(formatter=MergedOptionStringFormatter)
           }
