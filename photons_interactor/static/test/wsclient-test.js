0b008b65aa57f211dd62cc4278e9de2189c99716
Stephen Moore <stephen@delfick.com>
2018-07-13 21:38:42 +1000

    Some more websocket tests

--- photons_interactor/static/test/wsclient-test.js
+++ photons_interactor/static/test/wsclient-test.js
@@ -1,8 +1,9 @@
 import { createAction, createReducer } from "redux-act";
-import assert from "assert";
+import { assert } from "chai";
 import { listen } from "../js/wsclient.js";
 import WebSocket from "ws";
 import { END } from "redux-saga";
+import { put } from "redux-saga/effects";
 
 import { freePort, waitFor, makeTestStore, lineInfo } from "./helpers.js";
 import { WSCommand } from "../js/wsclient.js";
@@ -78,6 +79,29 @@ describe("WSClient", function() {
       }
     });
 
+    it("sagas can put inside a finally when a task is cancelled", async () => {
+      // This test is because we do a put inside a finally block in processWsReceive
+      // And I want to make sure that behaviour works as expected when the saga is cancelled
+
+      var original = GetData();
+
+      var saga = function*() {
+        try {
+          yield new Promise(() => {});
+        } finally {
+          yield put(GotData({ data: "PUT", original }));
+        }
+      };
+
+      var task = info.sagaMiddleware.run(saga);
+      await new Promise(resolve => setTimeout(resolve, 10));
+      assert(!original.payload.promise.getresolved());
+
+      task.cancel();
+      var res = await waitFor(original.payload.promise);
+      assert.deepEqual(res, { data: "PUT" });
+    });
+
     it("fails the command if we can't connect to the server", async () => {
       var original = GetData();
       var wscommand = WSCommand(
@@ -112,13 +136,19 @@ describe("WSClient", function() {
     before(async () => {
       port = await freePort();
       wsServer = new WebSocket.Server({ port });
+      var sleeps = [];
+
       wsServer.on("connection", socket => {
-        var send = (message_id, d) =>
-          socket.send(JSON.stringify({ ...d, message_id }));
+        var send = (message_id, d) => {
+          if (socket.readyState === 1) {
+            socket.send(JSON.stringify({ ...d, message_id }));
+          }
+        };
 
         actions.push("open");
 
         socket.on("close", () => {
+          sleeps.map(s => clearTimeout(s));
           actions.push("close");
         });
 
@@ -129,6 +159,15 @@ describe("WSClient", function() {
           var message_id = data.message_id;
           data = data.body;
           actions.push("message");
+
+          if (data.sleep) {
+            sleeps.push(
+              setTimeout(() => {
+                send(message_id, { reply: { slept: data.sleep } });
+              }, data.sleep)
+            );
+            return;
+          }
 
           if (data.stop) {
             send(message_id, { reply: { closing: true } });
@@ -141,6 +180,7 @@ describe("WSClient", function() {
           }
           if (data.multiple) {
             data.multiple.map(d => {
+              actions.push("multi");
               send(message_id, { reply: d });
             });
             return;
@@ -179,18 +219,139 @@ describe("WSClient", function() {
       assertActions("open", "message");
     });
 
-    it("reopens on close", async () => {
+    it("a message can timeout", async () => {
       var original = GetData();
       var wscommand = WSCommand(
         "/v1/test",
-        { stop: true },
+        { sleep: 1000 },
+        { original, onerror: GotError, onsuccess: GotData, timeout: 100 }
+      );
+      store.dispatch(wscommand);
+      var res = await waitFor(original.payload.promise);
+      assert.deepEqual(res, {
+        error: {
+          error: "Timedout waiting for a reply to the message",
+          error_code: "Timedout"
+        }
+      });
+      assertActions("message");
+    });
+
+    it("ignores multiple replies", async () => {
+      var original = GetData();
+      var wscommand = WSCommand(
+        "/v1/test",
+        { multiple: ["one", "two"] },
         { original, onerror: GotError, onsuccess: GotData }
       );
       store.dispatch(wscommand);
       var res = await waitFor(original.payload.promise);
+      assert.deepEqual(res, { data: "one" });
+      assertActions("message", "multi", "multi");
+    });
 
-      // Hack so the next message isn't given to our old connection
-      await new Promise(resolve => setTimeout(resolve, 50));
+    it("errors messages we're still waiting for when the server closes", async () => {
+      var original1 = GetData();
+      var wscommand = WSCommand(
+        "/v1/test",
+        { sleep: 50 },
+        { original: original1, onerror: GotError, onsuccess: GotData }
+      );
+      store.dispatch(wscommand);
+
+      var original2 = GetData();
+      var wscommand = WSCommand(
+        "/v1/test",
+        { stop: true },
+        { original: original2, onerror: GotError, onsuccess: GotData }
+      );
+      store.dispatch(wscommand);
+
+      await waitFor(original2.payload.promise);
+      var res = await waitFor(original1.payload.promise);
+      assert.deepEqual(res, {
+        error: {
+          error: "Lost connection to the server",
+          error_code: "LostConnection"
+        }
+      });
+      assertActions("message", "message");
+
+      // The lost connection error above means we will now be waiting for the next socket
+      // So waiting for another message is same as waiting for socket to be open again
+      var original3 = GetData();
+      var wscommand = WSCommand(
+        "/v1/test",
+        { five: "six" },
+        { original: original3, onerror: GotError, onsuccess: GotData }
+      );
+      store.dispatch(wscommand);
+      await waitFor(original3.payload.promise);
+      assertActions("close", "open", "message");
+    });
+
+    it("assigns out of order replies correctly", async () => {
+      var original1 = GetData();
+      var wscommand1 = WSCommand(
+        "/v1/test",
+        { sleep: 100 },
+        { original: original1, onerror: GotError, onsuccess: GotData }
+      );
+
+      var original2 = GetData();
+      var wscommand2 = WSCommand(
+        "/v1/test",
+        { sleep: 20 },
+        { original: original2, onerror: GotError, onsuccess: GotData }
+      );
+      store.dispatch(wscommand1);
+      store.dispatch(wscommand2);
+
+      var p1 = new Promise(resolve => {
+        waitFor(original1.payload.promise).then(res => {
+          assert.deepEqual(res, { data: { slept: 100 } });
+          resolve(Date.now());
+        });
+      });
+
+      var p2 = new Promise(resolve => {
+        waitFor(original2.payload.promise).then(res => {
+          assert.deepEqual(res, { data: { slept: 20 } });
+          resolve(Date.now());
+        });
+      });
+
+      var d1 = await waitFor(p1);
+      var d2 = await waitFor(p2);
+      assert.isAbove(d1 - d2, 0);
+      assert.isBelow(d1 - d2, 90);
+
+      assertActions("message", "message");
+    });
+
+    it("reopens on close", async () => {
+      var original1 = GetData();
+      var wscommand = WSCommand(
+        "/v1/test",
+        { sleep: 50 },
+        { original: original1, onerror: GotError, onsuccess: GotData }
+      );
+      store.dispatch(wscommand);
+
+      var original2 = GetData();
+      var wscommand = WSCommand(
+        "/v1/test",
+        { stop: true },
+        { original: original2, onerror: GotError, onsuccess: GotData }
+      );
+      store.dispatch(wscommand);
+
+      // Wait for the socket to close
+      var res = await waitFor(original2.payload.promise);
+      assert.deepEqual(res, { data: { closing: true } });
+
+      // Waiting for this to have failed means the next message goes to a new socket
+      await waitFor(original1.payload.promise);
 
       var original = GetData();
       var wscommand = WSCommand(
@@ -202,7 +363,7 @@ describe("WSClient", function() {
       var res = await waitFor(original.payload.promise);
       assert.deepEqual(res, { data: { three: "four" } });
 
-      assertActions("message", "close", "open", "message");
+      assertActions("message", "message", "close", "open", "message");
     });
   });
 });
