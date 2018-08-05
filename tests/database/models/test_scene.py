18ccc2a52ed53bd932141ff83fd917b1fe56f9be
Stephen Moore <stephen@delfick.com>
2018-08-05 16:29:17 +1000

    Tests for Scene and SceneInfo

---
+++ tests/database/models/test_scene.py
@@ -0,0 +1,88 @@
+# coding: spec
+
+from photons_interactor.database.test_helpers import DBTestRunner
+from photons_interactor.database.models.scene import Scene
+
+from photons_app.test_helpers import TestCase
+
+from noseOfYeti.tokeniser.support import noy_sup_setUp, noy_sup_tearDown
+from input_algorithms.meta import Meta
+import uuid
+
+test_runner = DBTestRunner()
+
+describe TestCase, "Scene":
+    describe "DelayedSpec":
+        it "returns a function that can be provided the uuid":
+            identifier = str(uuid.uuid1())
+
+            kwargs = dict(
+                  matcher = {"label": "den"}
+                , power = True
+                , color = "red"
+                , zones = [[0, 0, 0, 3500], [1, 1, 1, 3500]]
+                , chain = None
+                , duration = 1
+                )
+
+            scene = Scene.DelayedSpec(storing=False).normalise(Meta.empty(), kwargs)
+            normalised = scene(identifier)
+
+            expect = dict(kwargs)
+            expect["uuid"] = identifier
+
+            self.assertEqual(normalised.as_dict(), expect)
+
+            scene_for_storing = Scene.DelayedSpec(storing=True).normalise(Meta.empty(), kwargs)
+            normalised = scene_for_storing(identifier)
+
+            expect = dict(
+                  uuid = identifier
+                , matcher = '{"label": "den"}'
+                , power = True
+                , color = "red"
+                , zones = '[[0.0, 0.0, 0.0, 3500], [1.0, 1.0, 1.0, 3500]]'
+                , chain = None
+                , duration = 1
+                )
+
+            self.assertEqual(normalised.as_dict(), expect)
+
+    describe "Interaction with database":
+        before_each:
+            test_runner.before_each()
+            self.database = test_runner.database
+
+        after_each:
+            test_runner.after_each()
+
+        it "can store and retrieve from a spec":
+            identifier = str(uuid.uuid1())
+
+            chain = []
+            for i in range(5):
+                tile = []
+                for j in range(64):
+                    tile.append([j, 0, 1, 3500])
+                chain.append(tile)
+
+            kwargs = dict(
+                  uuid = identifier
+                , matcher = {"label": "den"}
+                , power = True
+                , color = "red"
+                , zones = [[0, 0, 0, 3500], [1, 1, 1, 3500]]
+                , chain = chain
+                , duration = 1
+                )
+
+            scene = Scene.Spec(storing=True).empty_normalise(**kwargs)
+            self.database.add(self.database.queries.create_scene(**scene))
+            self.database.commit()
+
+            got = self.database.queries.get_one_scene(uuid=identifier)
+            self.assertEqual(got.as_dict(), kwargs)
+
+            obj = got.as_object()
+            for prop in kwargs.keys():
+                self.assertEqual(getattr(obj, prop), kwargs[prop])
---
+++ tests/database/models/test_scene_info.py
@@ -0,0 +1,66 @@
+# coding: spec
+
+from photons_interactor.database.models.scene_info import SceneInfo
+from photons_interactor.database.test_helpers import DBTestRunner
+
+from photons_app.test_helpers import TestCase
+
+from noseOfYeti.tokeniser.support import noy_sup_setUp, noy_sup_tearDown
+from input_algorithms.meta import Meta
+import sqlalchemy.exc
+import uuid
+
+test_runner = DBTestRunner()
+
+describe TestCase, "SceneInfo":
+    it "can return itself as a dict":
+        info = SceneInfo(uuid="one", label=None, description=None)
+        self.assertEqual(info.as_dict(), {"uuid": "one"})
+
+        info = SceneInfo(uuid="two", label="kitchen", description=None)
+        self.assertEqual(info.as_dict(), {"uuid": "two", "label": "kitchen"})
+
+        info = SceneInfo(uuid="three", label="bathroom", description="blah")
+        self.assertEqual(info.as_dict()
+            , { "uuid": "three"
+              , "label": "bathroom"
+              , "description": "blah"
+              }
+            )
+
+    describe "Interaction with database":
+        before_each:
+            test_runner.before_each()
+            self.database = test_runner.database
+
+        after_each:
+            test_runner.after_each()
+
+        it "Must have unique uuid":
+            identifier = str(uuid.uuid1())
+
+            kwargs = dict(
+                  uuid = identifier
+                , label = "blah"
+                , description = "described"
+                )
+
+            info = self.database.queries.create_scene_info(**kwargs)
+            self.database.add(info)
+            self.database.commit()
+
+            info2 = self.database.queries.create_scene_info(**kwargs)
+            self.database.add(info2)
+            try:
+                with self.fuzzyAssertRaisesError(sqlalchemy.exc.IntegrityError):
+                    self.database.commit()
+            finally:
+                self.database.rollback()
+
+            info3 = self.database.queries.create_scene_info(uuid = identifier)
+            self.database.add(info3)
+            try:
+                with self.fuzzyAssertRaisesError(sqlalchemy.exc.IntegrityError):
+                    self.database.commit()
+            finally:
+                self.database.rollback()
