c4736b005704224ff49a9e9856ba85acdf72f560
Stephen Moore <stephen@delfick.com>
2018-09-08 18:40:45 +1000

    Update photons

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     , include_package_data = True
 
     , install_requires =
-      [ "lifx-photons-core==0.6"
+      [ "lifx-photons-core==0.6.1"
       , "tornado==5.0.2"
       , "SQLAlchemy==1.2.10"
       , "alembic==1.0.0"
