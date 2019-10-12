e19eca14902fef0f730f0d60e1f462fbd5d1ce15
Stephen Moore <stephen@delfick.com>
2019-10-12 20:51:31 +1100

    version bump and pin delfick_project

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
     , include_package_data = True
 
     , install_requires =
-      [ "lifx-photons-core==0.23.0"
+      [ "delfick_project==0.5.1"
+      , "lifx-photons-core==0.23.0"
       , "tornado==5.1.1"
       , "SQLAlchemy==1.3.3"
       , "alembic==1.0.0"
