6d1ebe44a1e111e6b024592b7d22ac772c08e6ef
Stephen Moore <stephen@delfick.com>
2020-01-11 23:46:29 +1100

    Update photons

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
 
     , install_requires =
       [ "delfick_project==0.7.3"
-      , "lifx-photons-core==0.24.4"
+      , "lifx-photons-core==0.24.5"
       , "tornado==5.1.1"
       , "SQLAlchemy==1.3.3"
       , "alembic==1.0.0"
